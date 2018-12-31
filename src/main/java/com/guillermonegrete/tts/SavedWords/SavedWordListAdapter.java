c36c26526d65606161f32469d76243377a0037d9
Memo <memito_095@hotmail.com>
2018-12-31 04:07:43 -0600

    Created saved words item layout and adapter, added placeholder data to recycler view

---
+++ src/main/java/com/guillermonegrete/tts/SavedWords/SavedWordListAdapter.java
@@ -0,0 +1,76 @@
+package com.guillermonegrete.tts.SavedWords;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.guillermonegrete.tts.R;
+import com.guillermonegrete.tts.db.Words;
+
+import java.util.List;
+
+public class SavedWordListAdapter extends RecyclerView.Adapter<SavedWordListAdapter.WordsViewHolder> {
+
+    private LayoutInflater layoutInflater;
+    private List<Words> wordsList;
+    private Context context;
+
+    public SavedWordListAdapter(Context context){
+        this.layoutInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+    public void setWordsList(List<Words> wordsList){
+        this.wordsList = wordsList;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public WordsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final View itemView = layoutInflater.inflate(R.layout.saved_word_item, parent, false);
+        return new WordsViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(WordsViewHolder holder, int position) {
+        if (wordsList == null) {
+            return;
+        }
+
+        final Words word = wordsList.get(position);
+        if (word != null) {
+            holder.wordText.setText(word.word);
+            holder.languageText.setText(word.lang);
+            holder.translationText.setText(word.definition);
+
+        }
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (wordsList == null) {
+            return 0;
+        } else {
+            return wordsList.size();
+        }
+    }
+
+    static class WordsViewHolder extends RecyclerView.ViewHolder{
+        private TextView wordText;
+        private TextView languageText;
+        private TextView translationText;
+
+        public WordsViewHolder(View itemView){
+            super(itemView);
+
+            wordText = (TextView) itemView.findViewById(R.id.saved_word_text);
+            languageText = (TextView) itemView.findViewById(R.id.saved_word_lang_text);
+            translationText = (TextView) itemView.findViewById(R.id.saved_word_translation_text);
+        }
+    }
+}
--- src/main/java/com/guillermonegrete/tts/SavedWordsFragment.java
+++ src/main/java/com/guillermonegrete/tts/SavedWordsFragment.java
@@ -1,20 +1,53 @@
 package com.guillermonegrete.tts;
 
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.guillermonegrete.tts.SavedWords.SavedWordListAdapter;
+import com.guillermonegrete.tts.SavedWords.WordsViewModel;
+import com.guillermonegrete.tts.db.Words;
+
+import java.util.List;
+
 public class SavedWordsFragment extends Fragment {
+
+    private SavedWordListAdapter wordListAdapter;
+    private WordsViewModel wordsViewModel;
+    private Context context;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+        wordListAdapter = new SavedWordListAdapter(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initData();
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         final View fragment_layout = inflater.inflate(R.layout.fragment_saved_words, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) fragment_layout.findViewById(R.id.recyclerview_saved_words);
+        recyclerView.setAdapter(wordListAdapter);
+        // recyclerView.addItemDecoration(new DividerItemDecoration(context, DividerItemDecoration.VERTICAL));
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
 
         fragment_layout.findViewById(R.id.new_word_button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +56,22 @@ public class SavedWordsFragment extends Fragment {
             }
         });
         return fragment_layout;
+    }
+
+    private void initData(){
+        wordsViewModel = ViewModelProviders.of(this).get(WordsViewModel.class);
+        wordsViewModel.getWordsList().observe(this, new Observer<List<Words>>() {
+            @Override
+            public void onChanged(@Nullable List<Words> movies) {
+                wordListAdapter.setWordsList(movies);
+            }
+        });
+    }
+
+    public void removeData() {
+        if (wordListAdapter != null) {
+            wordsViewModel.deleteAll();
+        }
     }
 
     private void showSaveDialog() {
