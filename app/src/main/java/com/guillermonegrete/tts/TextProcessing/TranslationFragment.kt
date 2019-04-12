776e74f07771bfb9148056ed72b1abbe124a4103
Memo <memito_095@hotmail.com>
2019-04-12 09:03:04 -0500

    Can copy definition from process text activity.

--- app/src/main/java/com/guillermonegrete/tts/TextProcessing/TranslationFragment.kt
+++ app/src/main/java/com/guillermonegrete/tts/TextProcessing/TranslationFragment.kt
@@ -1,9 +1,13 @@
 package com.guillermonegrete.tts.TextProcessing
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.guillermonegrete.tts.R
@@ -21,13 +25,19 @@ class TranslationFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_process_translation, container, false)
 
-        word?.let {
+        word?.let {word ->
             with(root){
-                findViewById<TextView>(R.id.saved_definition_text).text = it.definition
-                if(it.notes.isNullOrBlank()) {
+                findViewById<TextView>(R.id.saved_definition_text).text = word.definition
+                if(word.notes.isNullOrBlank()) {
                     findViewById<TextView>(R.id.saved_notes_label).visibility = View.GONE
                     findViewById<TextView>(R.id.saved_notes_text).visibility = View.GONE
-                }else findViewById<TextView>(R.id.saved_notes_text).text = it.notes
+                }else findViewById<TextView>(R.id.saved_notes_text).text = word.notes
+
+                findViewById<ImageButton>(R.id.copy_definition_button).setOnClickListener {
+                    val clipboard = activity?.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                    val clip: ClipData = ClipData.newPlainText("simple text", word.definition)
+                    clipboard.primaryClip = clip
+                }
             }
         }
 
