2f3a3f64964b03d02030aaa1decec4d5f1b37631
Memo <memito_095@hotmail.com>
2019-06-10 01:19:59 -0500

    Added app shortcuts for overlay and clipboard mode, also fixed permission bug for api >= 23.

--- app/src/main/java/com/guillermonegrete/tts/main/AcquireScreenshotPermission.kt
+++ app/src/main/java/com/guillermonegrete/tts/main/AcquireScreenshotPermission.kt
@@ -4,9 +4,14 @@
 package com.guillermonegrete.tts.main
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.media.projection.MediaProjectionManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
 
 import com.guillermonegrete.tts.Services.ScreenTextService
 
@@ -17,25 +22,44 @@ class AcquireScreenshotPermission : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val mediaProjectionManager = this.getSystemService(MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
-        startActivityForResult(mediaProjectionManager.createScreenCaptureIntent(), REQUEST_CODE_SCREEN_CAPTURE)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
+            startActivityForResult(intent, REQUEST_CODE_DRAW_OVERLAY)
+        } else {
+            getScreenCaptureIntent()
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CODE_SCREEN_CAPTURE) {
-            if (resultCode == RESULT_OK) {
-                val intent = Intent(this, ScreenTextService::class.java)
-                intent.action = NORMAL_SERVICE
-                intent.putExtra(ScreenTextService.EXTRA_RESULT_CODE, resultCode)
-                intent.putExtras(data)
-                startService(intent)
+        if (resultCode == AppCompatActivity.RESULT_OK && data != null) {
+            when(requestCode) {
+                REQUEST_CODE_SCREEN_CAPTURE -> {
+                    val intent = Intent(this, ScreenTextService::class.java)
+                    intent.action = NORMAL_SERVICE
+                    intent.putExtra(ScreenTextService.EXTRA_RESULT_CODE, resultCode)
+                    intent.putExtras(data)
+                    startService(intent)
+                    finish()
+                }
+                REQUEST_CODE_DRAW_OVERLAY -> {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
+                        getScreenCaptureIntent()
+                    } else {
+                        finish()
+                    }
+                }
             }
         }
-        finish()
+    }
+
+    private fun getScreenCaptureIntent(){
+        val manager = getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        startActivityForResult(manager.createScreenCaptureIntent(), REQUEST_CODE_SCREEN_CAPTURE)
     }
 
     companion object {
         const val REQUEST_CODE_SCREEN_CAPTURE = 200
+        const val REQUEST_CODE_DRAW_OVERLAY = 400
     }
 }
