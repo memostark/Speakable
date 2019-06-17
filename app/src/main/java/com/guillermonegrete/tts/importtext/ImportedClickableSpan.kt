96ba340a118d22ceddebc8b2cd7c64046bc8b2a0
Memo <memito_095@hotmail.com>
2019-06-16 23:27:06 -0500

    Implemented view pager for the imported text in visualize activity.

--- app/src/main/java/com/guillermonegrete/tts/importtext/ImportedClickableSpan.kt
+++ app/src/main/java/com/guillermonegrete/tts/importtext/ImportedClickableSpan.kt
@@ -5,13 +5,20 @@ import android.graphics.Color
 import android.text.TextPaint
 import android.text.style.ClickableSpan
 import android.view.View
-import android.widget.Toast
+import android.widget.TextView
 import com.guillermonegrete.tts.textprocessing.ProcessTextActivity
 
 class ImportedClickableSpan(val word: String): ClickableSpan() {
 
+    var clicked = false
+    private var highLightColor = 0x6633B5E5
+
     override fun onClick(widget: View) {
-        Toast.makeText(widget.context, word, Toast.LENGTH_SHORT).show()
+        val textview = widget as TextView
+        highLightColor = textview.highlightColor
+        clicked = true
+        widget.invalidate()
+
         val context = widget.context
         val wiktionaryIntent = Intent(context, ProcessTextActivity::class.java)
         wiktionaryIntent.action = ProcessTextActivity.NO_SERVICE
@@ -20,7 +27,9 @@ class ImportedClickableSpan(val word: String): ClickableSpan() {
     }
 
     override fun updateDrawState(ds: TextPaint) {
-        ds.color = Color.BLACK
-        ds.isUnderlineText = false
+        ds.bgColor = if(clicked)  highLightColor else Color.TRANSPARENT
+        clicked = false
     }
+
+
 }
