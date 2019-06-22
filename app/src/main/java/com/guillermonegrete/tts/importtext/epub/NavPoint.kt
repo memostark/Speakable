536c960053d28c18d56a2d1bd5b97e12943bbfaf
Memo <memito_095@hotmail.com>
2019-06-22 17:26:13 -0500

    Can change section using the table of contents.

--- app/src/main/java/com/guillermonegrete/tts/importtext/epub/NavPoint.kt
+++ app/src/main/java/com/guillermonegrete/tts/importtext/epub/NavPoint.kt
@@ -1,3 +1,9 @@
 package com.guillermonegrete.tts.importtext.epub
 
-data class NavPoint(val navLabel: String, val content: String)
+data class NavPoint(val navLabel: String, val content: String){
+
+    fun getContentWithoutTag(): String{
+        val tagIndex = content.indexOf("#")
+        return if(tagIndex > 0) content.substring(0, tagIndex) else content
+    }
+}
