0f90051bb89c30d48fd3f93872dd93fb767c00c0
Memo <memito_095@hotmail.com>
2019-11-20 23:37:03 -0600

    Implemented dark theme for the whole application.

--- app/src/main/java/com/guillermonegrete/tts/SpeakableApplication.kt
+++ app/src/main/java/com/guillermonegrete/tts/SpeakableApplication.kt
@@ -3,8 +3,20 @@ package com.guillermonegrete.tts
 import com.guillermonegrete.tts.di.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import androidx.preference.PreferenceManager
+import com.guillermonegrete.tts.main.SettingsFragment
+import com.guillermonegrete.tts.utils.DEFAULT_MODE
+import com.guillermonegrete.tts.utils.applyTheme
+
 
 class SpeakableApplication: DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val themePref = sharedPreferences.getString(SettingsFragment.PREF_THEME, DEFAULT_MODE)
+        themePref?.let { applyTheme(themePref) }
+    }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerApplicationComponent.factory().create(this)
