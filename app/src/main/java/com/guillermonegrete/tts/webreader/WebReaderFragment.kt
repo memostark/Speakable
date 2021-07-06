894b5cc8a3dbf0b1402aeec7e3c54105118e7a82
Memo <memito_095@hotmail.com>
2021-07-05 22:33:15 -0500

    Changed text size and show text info dialog on clicked word.

--- app/src/main/java/com/guillermonegrete/tts/webreader/WebReaderFragment.kt
+++ app/src/main/java/com/guillermonegrete/tts/webreader/WebReaderFragment.kt
@@ -1,8 +1,10 @@
 package com.guillermonegrete.tts.webreader
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import android.text.Html
+import android.view.MotionEvent
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -10,6 +12,7 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.guillermonegrete.tts.R
 import com.guillermonegrete.tts.databinding.FragmentWebReaderBinding
+import com.guillermonegrete.tts.textprocessing.TextInfoDialog
 
 class WebReaderFragment : Fragment(R.layout.fragment_web_reader){
 
@@ -20,6 +23,9 @@ class WebReaderFragment : Fragment(R.layout.fragment_web_reader){
 
     val args: WebReaderFragmentArgs by navArgs()
 
+    private var clickedWord: String? = null
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         (activity as? AppCompatActivity)?.supportActionBar?.hide()
         _binding = FragmentWebReaderBinding.bind(view)
@@ -30,6 +36,19 @@ class WebReaderFragment : Fragment(R.layout.fragment_web_reader){
             } else {
                 Html.fromHtml(it)
             }
+
+            binding.bodyText.setOnTouchListener { _, event ->
+                if (event.action == MotionEvent.ACTION_DOWN) {
+                    val offset = binding.bodyText.getOffsetForPosition(event.x, event.y)
+                    clickedWord = findWordForRightHanded(binding.bodyText.text.toString(), offset)
+                }
+                return@setOnTouchListener false
+            }
+
+            binding.bodyText.setOnClickListener {
+                clickedWord?.let { word -> showTextDialog(word) }
+                clickedWord = null
+            }
         })
 
         viewModel.loadDoc(args.link)
@@ -38,5 +57,53 @@ class WebReaderFragment : Fragment(R.layout.fragment_web_reader){
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    private fun showTextDialog(text: CharSequence){
+        // TODO try to add this as destination in jetpack navigation
+        val dialog = TextInfoDialog.newInstance(
+            text.toString(),
+            TextInfoDialog.NO_SERVICE,
+            null,
+        )
+        dialog.show(childFragmentManager, "Text_info")
+    }
+
+    private fun findWordForRightHanded(
+        str: String,
+        offset: Int
+    ): String { // when you touch ' ', this method returns left word.
+        var newOffset = offset
+        if (str.length == newOffset) {
+            newOffset-- // without this code, you will get exception when touching end of the text
+        }
+        if (str[newOffset] == ' ') {
+            newOffset--
+        }
+        var startIndex = newOffset
+        var endIndex = newOffset
+        try {
+            while (str[startIndex] != ' ' && str[startIndex] != '\n') {
+                startIndex--
+            }
+        } catch (e: StringIndexOutOfBoundsException) {
+            startIndex = 0
+        }
+        try {
+            while (str[endIndex] != ' ' && str[endIndex] != '\n') {
+                endIndex++
+            }
+        } catch (e: StringIndexOutOfBoundsException) {
+            endIndex = str.length
+        }
+
+        // without this code, you will get 'here!' instead of 'here'
+        // if you use only english, just check whether this is alphabet,
+        // but 'I' use korean, so i use below algorithm to get clean word.
+        val last = str[endIndex - 1]
+        if (last == ',' || last == '.' || last == '!' || last == '?' || last == ':' || last == ';') {
+            endIndex--
+        }
+        return str.substring(startIndex, endIndex)
     }
 }
