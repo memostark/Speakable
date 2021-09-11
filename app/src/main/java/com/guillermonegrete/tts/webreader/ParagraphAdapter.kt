9c528658caf63ef0a8ba626d2ca3c01e7f24dd16
Memo <memito_095@hotmail.com>
2021-09-10 23:28:01 -0500

    Created a new view holder for the expanded item in web reader, improved the layout.

--- app/src/main/java/com/guillermonegrete/tts/webreader/ParagraphAdapter.kt
+++ app/src/main/java/com/guillermonegrete/tts/webreader/ParagraphAdapter.kt
@@ -2,32 +2,58 @@ package com.guillermonegrete.tts.webreader
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.view.isInvisible
-import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.guillermonegrete.tts.R
+import com.guillermonegrete.tts.databinding.ParagraphExpandedItemBinding
 import com.guillermonegrete.tts.databinding.ParagraphItemBinding
 import com.guillermonegrete.tts.db.Words
 
 class ParagraphAdapter(
     val items: List<Words>,
     val viewModel: WebReaderViewModel
-): RecyclerView.Adapter<ParagraphAdapter.ViewHolder>() {
+): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private var expandedItemPos = -1
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        return ViewHolder(ParagraphItemBinding.inflate(inflater, parent, false))
+        return if(viewType == R.layout.paragraph_expanded_item) ExpandedViewHolder(ParagraphExpandedItemBinding.inflate(inflater, parent, false))
+            else ViewHolder(ParagraphItemBinding.inflate(inflater, parent, false))
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(items[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder){
+            is ViewHolder -> holder.bind(items[position])
+            is ExpandedViewHolder -> holder.bind(items[position])
+        }
     }
 
     override fun getItemCount() = items.size
 
+    override fun getItemViewType(position: Int) = if(expandedItemPos == position) R.layout.paragraph_expanded_item else R.layout.paragraph_item
+
     inner class ViewHolder(val binding: ParagraphItemBinding): RecyclerView.ViewHolder(binding.root){
+
+        init {
+            with(binding){
+
+                toggleParagraph.setOnClickListener {
+                    val previousExpandedPos = expandedItemPos
+                    val isExpanded = adapterPosition == expandedItemPos
+                    expandedItemPos = if(isExpanded) -1 else adapterPosition
+                    notifyItemChanged(previousExpandedPos)
+                    notifyItemChanged(adapterPosition)
+                }
+            }
+        }
+
+        fun bind(item: Words){
+            binding.paragraph.text = item.word
+        }
+
+    }
+
+    inner class ExpandedViewHolder(val binding: ParagraphExpandedItemBinding): RecyclerView.ViewHolder(binding.root){
 
         val noTranslationText: CharSequence = itemView.context.getText(R.string.paragraph_not_translated)
 
@@ -50,11 +76,6 @@ class ParagraphAdapter(
 
         fun bind(item: Words){
             binding.paragraph.text = item.word
-
-            val isExpanded = adapterPosition == expandedItemPos
-            binding.translate.isInvisible = !isExpanded
-            binding.translatedParagraph.isVisible = isExpanded
-            binding.toggleParagraph.setIconResource(if(isExpanded) R.drawable.ic_baseline_arrow_drop_down_24 else R.drawable.ic_baseline_arrow_right_24)
             binding.translatedParagraph.text = if(item.definition.isNotBlank()) item.definition else noTranslationText
         }
 
