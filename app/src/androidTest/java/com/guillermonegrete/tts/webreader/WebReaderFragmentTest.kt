bed01699f00e4099e47f56bb95643450214b45ea
Memo <memito_095@hotmail.com>
2022-10-22 23:59:56 -0500

    Added MockWebServer, used initially for web reader fragment tests.

--- app/src/androidTest/java/com/guillermonegrete/tts/webreader/WebReaderFragmentTest.kt
+++ app/src/androidTest/java/com/guillermonegrete/tts/webreader/WebReaderFragmentTest.kt
@@ -1,14 +1,17 @@
 package com.guillermonegrete.tts.webreader
 
 import androidx.core.os.bundleOf
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import androidx.test.platform.app.InstrumentationRegistry
 import com.guillermonegrete.tts.R
 import com.guillermonegrete.tts.di.WordRepositorySourceModule
 import com.guillermonegrete.tts.launchFragmentInHiltContainer
@@ -16,12 +19,14 @@ import com.guillermonegrete.tts.utils.EspressoIdlingResource
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
-import org.hamcrest.Matchers.not
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @MediumTest
 @RunWith(AndroidJUnit4::class)
@@ -31,6 +36,19 @@ class WebReaderFragmentTest{
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
+
+    private lateinit var server: MockWebServer
+
+    @Before
+    fun setup(){
+        server = MockWebServer()
+        server.start()
+    }
+
+    @After
+    fun teardown(){
+        server.shutdown()
+    }
 
     @Before
     fun registerIdlingResource() {
@@ -44,9 +62,21 @@ class WebReaderFragmentTest{
 
     @Test
     fun given_default_layout_when_toggle_button_then_paragraph_layout(){
-        val args = bundleOf("link" to "https://en.wikipedia.org/wiki/Wiktionary")
+        val body = readPage()
+        server.enqueue(MockResponse().setBody(body))
+
+        val args = bundleOf("link" to server.url("/").toString())
         launchFragmentInHiltContainer<WebReaderFragment>(args, R.style.AppTheme)
 
         onView(withId(R.id.paragraphs_list)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.paragraphs_list))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click())
+            )
     }
+
+    private fun readPage() =
+        InstrumentationRegistry.getInstrumentation()
+            .context.assets.open("test_epub.epub").bufferedReader().use { it.readText() }
 }
