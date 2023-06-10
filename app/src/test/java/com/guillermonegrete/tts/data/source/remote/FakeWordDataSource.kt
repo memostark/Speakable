91346fd4b2ab0393ace8308173340e17b3a7811c
Memo <memito_095@hotmail.com>
2023-06-09 23:48:09 -0600

    Improved update method for the save word dialog viewmodel. Created tests for it and achieved full line coverage.

--- app/src/test/java/com/guillermonegrete/tts/data/source/remote/FakeWordDataSource.kt
+++ app/src/test/java/com/guillermonegrete/tts/data/source/remote/FakeWordDataSource.kt
@@ -4,12 +4,11 @@ import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.LiveData
 import com.guillermonegrete.tts.data.source.WordDataSource
 import com.guillermonegrete.tts.db.Words
-import java.lang.Exception
 import java.util.LinkedHashMap
 
 class FakeWordDataSource: WordDataSource {
 
-    var translationsData: LinkedHashMap<String, Words> = LinkedHashMap()
+    var translationsData: LinkedHashMap<Int, Words> = LinkedHashMap()
 
     override fun getWords(): MutableList<Words> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -23,8 +22,10 @@ class FakeWordDataSource: WordDataSource {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun insertWords(vararg words: Words?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun insertWords(vararg words: Words) {
+        for(word in words) {
+            translationsData[word.id] = word
+        }
     }
 
     override fun deleteWords(vararg words: Words?) {
@@ -40,22 +41,22 @@ class FakeWordDataSource: WordDataSource {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getWordLanguageInfo(
-        wordText: String,
-        languageFrom: String,
-        languageTo: String
-    ): Words {
-        return translationsData[wordText] ?: throw Exception("Not found")
-    }
-
     override fun loadWord(word: String?, language: String?): LiveData<Words> {
         TODO("Not yet implemented")
+    }
+
+    override fun update(word: Words): Int {
+        if (translationsData.containsKey(word.id)){
+            translationsData[word.id] = word
+            return 1
+        }
+        return 0
     }
 
     @VisibleForTesting
     fun addTranslation(vararg words: Words) {
         for (word in words) {
-            translationsData[word.word] = word
+            translationsData[word.id] = word
         }
     }
 }
