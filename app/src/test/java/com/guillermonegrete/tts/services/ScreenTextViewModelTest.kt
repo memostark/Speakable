92a05fef7ba4dd439216bbbf72d73affb2e52c87
Memo <memito_095@hotmail.com>
2023-06-11 18:19:39 -0600

    Added test cases for image detection error. ScreenTextViewModel has now full line coverage.

--- app/src/test/java/com/guillermonegrete/tts/services/ScreenTextViewModelTest.kt
+++ app/src/test/java/com/guillermonegrete/tts/services/ScreenTextViewModelTest.kt
@@ -2,7 +2,6 @@ package com.guillermonegrete.tts.services
 
 import android.content.SharedPreferences
 import android.graphics.Rect
-import android.graphics.RectF
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.guillermonegrete.tts.MainCoroutineRule
 import com.guillermonegrete.tts.TestThreadExecutor
@@ -25,21 +24,20 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
 import io.mockk.slot
 import io.mockk.verify
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.io.IOException
 import java.lang.RuntimeException
 
+@ExperimentalCoroutinesApi
 class ScreenTextViewModelTest {
 
     private lateinit var viewModel: ScreenTextViewModel
-
-    private lateinit var detectTextInteractor: DetectTextFromScreen
-
-
 
     @get:Rule
     var mainCoroutineRule = MainCoroutineRule(UnconfinedTestDispatcher())
@@ -53,6 +51,10 @@ class ScreenTextViewModelTest {
     private lateinit var tts: CustomTTS
     @MockK
     private lateinit var preferences: SharedPreferences
+    @MockK(relaxed = true)
+    private lateinit var imageCaptor: ScreenImageCaptor
+    @MockK
+    private lateinit var rect: Rect
 
     private lateinit var wordRepository: FakeWordRepository
 
@@ -76,22 +78,10 @@ class ScreenTextViewModelTest {
     }
 
     @Test
-    fun `When play clicked, then language detected and palying state changes`() {
-        val imageCaptor = mockk<ScreenImageCaptor>(relaxed = true)
-        val rect = mockk<Rect>()
+    fun `When play clicked, then language detected and playing state changes`() {
         viewModel.onPlayClick(imageCaptor, rect)
 
-        val slot = slot<ScreenImageCaptor.Callback>()
-        verify { imageCaptor.getImage(rect, capture(slot)) }
-
-        assertEquals(PlayAudioState.Loading, viewModel.playingAudio.getOrAwaitValue())
-
-        slot.captured.onImageCaptured(mockk())
-
-        val imageProcessingSlot = slot<ImageProcessingSource.Callback>()
-        verify { imageProcessor.detectText(any(), capture(imageProcessingSlot)) }
-
-        imageProcessingSlot.captured.onTextDetected("Detected text", "en")
+        getDetectedText()
 
         assertEquals("en", viewModel.langDetected.getOrAwaitValue())
 
@@ -104,22 +94,20 @@ class ScreenTextViewModelTest {
     }
 
     @Test
-    fun `Given tts playing, when reproduce clicked, then tts stopped`() {
-        val imageCaptor = mockk<ScreenImageCaptor>(relaxed = true)
-        val rect = mockk<Rect>()
+    fun `Given detection error, when play clicked, then error state`() {
         viewModel.onPlayClick(imageCaptor, rect)
 
-        val slot = slot<ScreenImageCaptor.Callback>()
-        verify { imageCaptor.getImage(rect, capture(slot)) }
+        val error = IOException("Error reading bitmap")
+        setDetectedTextError(error)
 
-        assertEquals(PlayAudioState.Loading, viewModel.playingAudio.getOrAwaitValue())
+        assertEquals(PlayAudioState.Error(error), viewModel.playingAudio.getOrAwaitValue())
+    }
 
-        slot.captured.onImageCaptured(mockk())
+    @Test
+    fun `Given tts playing, when reproduce clicked, then tts stopped`() {
+        viewModel.onPlayClick(imageCaptor, rect)
 
-        val imageProcessingSlot = slot<ImageProcessingSource.Callback>()
-        verify { imageProcessor.detectText(any(), capture(imageProcessingSlot)) }
-
-        imageProcessingSlot.captured.onTextDetected("Detected text", "en")
+        getDetectedText()
 
         assertEquals("en", viewModel.langDetected.getOrAwaitValue())
 
@@ -133,19 +121,43 @@ class ScreenTextViewModelTest {
     }
 
     @Test
+    fun `Given language unavailable, When play clicked, then error`() {
+        viewModel.onPlayClick(imageCaptor, rect)
+
+        getDetectedText()
+
+        val ttsListener = getTTSListener()
+        ttsListener.onLanguageUnavailable()
+
+        val error = viewModel.playingAudio.getOrAwaitValue() as PlayAudioState.Error
+        assertEquals("TTS error: Language Unavailable", error.exception.message)
+    }
+
+    @Test
+    fun `Given tts error, when play clicked, then error`() {
+        viewModel.onPlayClick(imageCaptor, rect)
+
+        getDetectedText()
+
+        val ttsListener = getTTSListener()
+        ttsListener.onError()
+
+        val error = viewModel.playingAudio.getOrAwaitValue() as PlayAudioState.Error
+        assertEquals("TTS error: Failed to play", error.exception.message)
+    }
+
+    @Test
     fun `When translate click, then load success`() {
         val translation = Translation(listOf(Segment("translated text", "Detected text")), "ES")
         wordRepository.addTranslation(translation)
 
-        val imageCaptor = mockk<ScreenImageCaptor>(relaxed = true)
-        val rect = mockk<Rect>()
         viewModel.onTranslateClick(imageCaptor, rect)
 
         assertEquals(LoadResult.Loading, viewModel.textTranslated.getOrAwaitValue())
 
         every { preferences.getInt(SettingsFragment.PREF_LANGUAGE_TO, 15) } returns 1
 
-        getDetectedText(imageCaptor, rect)
+        getDetectedText()
 
         assertEquals(1, viewModel.langToPreference.getOrAwaitValue())
         assertEquals(LoadResult.Success(translation), viewModel.textTranslated.getOrAwaitValue())
@@ -153,18 +165,13 @@ class ScreenTextViewModelTest {
 
     @Test
     fun `Given no translations, when translate click, then error`() {
-        val translation = Translation(listOf(Segment("translated text", "Detected text")), "ES")
-//        wordRepository.addTranslation(translation)
-
-        val imageCaptor = mockk<ScreenImageCaptor>(relaxed = true)
-        val rect = mockk<Rect>()
         viewModel.onTranslateClick(imageCaptor, rect)
 
         assertEquals(LoadResult.Loading, viewModel.textTranslated.getOrAwaitValue())
 
         every { preferences.getInt(SettingsFragment.PREF_LANGUAGE_TO, 15) } returns 1
 
-        getDetectedText(imageCaptor, rect)
+        getDetectedText()
 
         assertEquals(1, viewModel.langToPreference.getOrAwaitValue())
         val error = viewModel.textTranslated.getOrAwaitValue() as LoadResult.Error
@@ -172,11 +179,21 @@ class ScreenTextViewModelTest {
     }
 
     @Test
-    fun `Detect language and translate`() {
+    fun `Given detection error, when translate click, then error`() {
+        val translation = Translation(listOf(Segment("translated text", "Detected text")), "ES")
+        wordRepository.addTranslation(translation)
 
+        viewModel.onTranslateClick(imageCaptor, rect)
+
+        assertEquals(LoadResult.Loading, viewModel.textTranslated.getOrAwaitValue())
+
+        val error = IOException("Error reading bitmap")
+        setDetectedTextError(error)
+
+        assertEquals(LoadResult.Error<Translation>(error), viewModel.textTranslated.getOrAwaitValue())
     }
 
-    private fun getDetectedText(imageCaptor: ScreenImageCaptor, rect: Rect) {
+    private fun getDetectedText() {
         val slot = slot<ScreenImageCaptor.Callback>()
         verify { imageCaptor.getImage(rect, capture(slot)) }
 
@@ -186,6 +203,18 @@ class ScreenTextViewModelTest {
         verify { imageProcessor.detectText(any(), capture(imageProcessingSlot)) }
 
         imageProcessingSlot.captured.onTextDetected("Detected text", "en")
+    }
+
+    private fun setDetectedTextError(error: Exception) {
+        val slot = slot<ScreenImageCaptor.Callback>()
+        verify { imageCaptor.getImage(rect, capture(slot)) }
+
+        slot.captured.onImageCaptured(mockk())
+
+        val imageProcessingSlot = slot<ImageProcessingSource.Callback>()
+        verify { imageProcessor.detectText(any(), capture(imageProcessingSlot)) }
+
+        imageProcessingSlot.captured.onFailure(error)
     }
 
     private fun getTTSListener(): CustomTTS.Listener {
