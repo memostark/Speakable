484a1f265be44b01b3ab0b448b027eca80f2bebe
Memo <memito_095@hotmail.com>
2025-03-29 23:46:16 -0600

    Fixed bubble view disappearing during config change if the position was bigger than the new screen size. Handle insets of the nav bar on landscape mode.

--- app/src/main/java/com/guillermonegrete/tts/utils/ContextExt.kt
+++ app/src/main/java/com/guillermonegrete/tts/utils/ContextExt.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.content.res.Resources.getSystem
 import android.graphics.Point
 import android.os.Build
+import android.view.Surface
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.annotation.RequiresApi
+import androidx.core.graphics.Insets
 import timber.log.Timber
 
 fun Context.dpToPixel(dp: Int): Int {
@@ -19,17 +21,18 @@ val Context.actionBarSize
     get() = theme.obtainStyledAttributes(intArrayOf(android.R.attr.actionBarSize))
         .let { attrs -> attrs.getDimension(0, 0F).toInt().also { attrs.recycle() } }
 
-fun Context.getScreenSizes(): ScreenInfo {
+fun Context.getScreenSizes(allBars: Boolean = false): ScreenInfo {
     val wm = getSystemService(Context.WINDOW_SERVICE) as WindowManager
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
         val windowMetrics = wm.currentWindowMetrics
         val windowInsets = windowMetrics.windowInsets
 
-        val insets = windowInsets.getInsets(WindowInsets.Type.navigationBars() or WindowInsets.Type.displayCutout())
+        val bars = if (allBars) WindowInsets.Type.systemBars() else WindowInsets.Type.navigationBars()
+        val insets = windowInsets.getInsets(bars or WindowInsets.Type.displayCutout())
         val b = windowMetrics.bounds
         Timber.d("Insets: $insets")
 
-        ScreenInfo(b.width(), b.height(), insets.top, insets.bottom)
+        ScreenInfo(b.width(), b.height(), Insets.toCompatInsets(insets))
     } else @Suppress("DEPRECATION", "InternalInsetResource", "DiscouragedApi") {
         val display = wm.defaultDisplay // deprecated in API 30
         val realSize = Point()
@@ -44,7 +47,7 @@ fun Context.getScreenSizes(): ScreenInfo {
             statusHeight = resources.getDimensionPixelSize(resourceId)
         }
 
-        ScreenInfo(realSize.x, realSize.y, statusHeight, realSize.y - usableSize.y)
+        ScreenInfo(realSize.x, realSize.y, Insets.of(0, statusHeight, 0, realSize.y - usableSize.y))
     }
 }
 
@@ -56,9 +59,34 @@ fun Context.statusBarVisible(): Boolean {
     return windowInsets.isVisible(WindowInsets.Type.statusBars())
 }
 
+fun Context.navBarInsets(): Insets {
+    val wm = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        val windowMetrics = wm.currentWindowMetrics
+        val windowInsets = windowMetrics.windowInsets
+
+        val insets = windowInsets.getInsets(WindowInsets.Type.navigationBars())
+        Timber.d("Insets: $insets")
+
+        Insets.toCompatInsets(insets)
+    } else @Suppress("DEPRECATION", "InternalInsetResource", "DiscouragedApi") {
+        val display = wm.defaultDisplay // deprecated in API 30
+        val realSize = Point()
+        display?.getRealSize(realSize) // deprecated in API 30
+        val usableSize = Point()
+        display?.getSize(usableSize)
+        Timber.d("Real: $realSize, Usable: $usableSize, rotation: ${display.rotation}")
+
+        when(display.rotation) {
+            Surface.ROTATION_90 -> Insets.of(0, 0, realSize.x - usableSize.x, realSize.y - usableSize.y)
+            Surface.ROTATION_270 -> Insets.of(realSize.x - usableSize.x, 0, 0, realSize.y - usableSize.y)
+            else -> Insets.of(0, 0, 0, realSize.y - usableSize.y)
+        }
+    }
+}
+
 data class ScreenInfo(
     val width: Int,
     val height: Int,
-    val statusHeight: Int,
-    val navHeight: Int,
+    val insets: Insets,
 )
